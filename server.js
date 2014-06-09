3556f582afe93fbd2115bd493ff88888754589aa
Damon Black <damon.black@gmail.com>
2014-06-10 04:25:22 +1000

    server fix

--- server.js
+++ server.js
@@ -2,23 +2,22 @@
 var app = require('./app.js');
 var debug = require('debug')('adb');
 
-app.set('ipaddress', process.env.OPENSHIFT_NODEJS_IP);
+var ipaddress = process.env.OPENSHIFT_NODEJS_IP;
+if (typeof app.ipaddress === "undefined") {
+    //  Log errors on OpenShift but continue w/ 127.0.0.1 - this
+    //  allows us to run/test the app locally.
+    console.warn('No OPENSHIFT_NODEJS_IP var, using 127.0.0.1');
+    ipaddress = "127.0.0.1";
+};
 
-        if (typeof app.ipaddress === "undefined") {
-            //  Log errors on OpenShift but continue w/ 127.0.0.1 - this
-            //  allows us to run/test the app locally.
-            console.warn('No OPENSHIFT_NODEJS_IP var, using 127.0.0.1');
-            app.set('ipaddress',"127.0.0.1");
-        };
-
-app.set('port', process.env.OPENSHIFT_NODEJS_PORT || 8080);
+var port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
 
 // var server = app.listen(app.get('port'), function() {
 // 	console.log('we on bish');
 // 	debug('Express server listening on port ' + server.address().port);
 // })
 
-var server = app.listen(app.port, app.ipaddress, function() {
-            console.log('%s: Node server started on %s:%d ...',
-                        Date(Date.now() ), app.ipaddress, app.port);
-        });
+var server = app.listen(port, ipaddress, function() {
+    console.log('%s: Node server started on %s:%d ...',
+                Date(Date.now() ), ipaddress, port);
+});
