54a965a46d9565cfaa2bc642a4dba305fd07c3b5
Damon Black <damon.black@gmail.com>
2014-06-10 03:42:28 +1000

    fix port

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 var app = require('./app.js');
 var debug = require('debug')('adb');
 
-app.set('port', process.env.PORT || 3000);
+app.set('port', process.env.OPENSHIFT_NODEJS_PORT || 8080);
 
 var server = app.listen(app.get('port'), function() {
 	console.log('we on bish');
