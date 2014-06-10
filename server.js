f588c2eab6adcffbd1f7fe0a7c28a61c590da1ee
Damon Black <damon.black@gmail.com>
2014-06-10 13:10:34 +1000

    env setups

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 #!/usr/bin/env node
 var app = require('./app.js');
-var debug = require('debug')('adb');
 
-var ipaddress = process.env.OPENSHIFT_NODEJS_IP;
+
+var ipaddress = process.env.OPENSHIFT_NODEJS_IP ;
 if (typeof ipaddress === "undefined") {
     //  Log errors on OpenShift but continue w/ 127.0.0.1 - this
     //  allows us to run/test the app locally.
@@ -10,7 +10,10 @@ if (typeof ipaddress === "undefined") {
     ipaddress = "127.0.0.1";
 };
 
-var port = process.env.OPENSHIFT_NODEJS_PORT ||  process.env.OPENSHIFT_INTERNAL_PORT || 8080; 
+var port = process.env.OPENSHIFT_NODEJS_PORT ||  process.env.OPENSHIFT_INTERNAL_PORT || 8080;
+if (app.get('env') === 'development') {
+	var port = 4000;
+}
 
 // var server = app.listen(app.get('port'), function() {
 // 	console.log('we on bish');
